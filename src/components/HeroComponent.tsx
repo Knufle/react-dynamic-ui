de352649a38f110b4905564cee6dd390232f07fc
Knufle <iamknufle@gmail.com>
2025-04-01 16:54:07 -0300

    fix: change HeroComponent export to default for consistency

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -5,7 +5,7 @@ interface HeroComponentProps {
   style?: React.CSSProperties;
 }
 
-export function HeroComponent({ title, subtitle, ctaText, style }: HeroComponentProps) {
+export default function HeroComponent({ title, subtitle, ctaText, style }: HeroComponentProps) {
   return (
     <div style={{ textAlign: 'center', padding: '4rem 2rem', ...style }}>
       <h1 style={{ fontSize: '3rem', marginBottom: '1rem' }}>{title}</h1>
